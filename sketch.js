bbdb5a48aba67ae4b1716dafabfcd6db6932f432
goru155 <74966689+goru155@users.noreply.github.com>
2021-09-10 17:19:29 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -5,6 +5,8 @@ const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, box2, box3,box4,box5,box6,box7,box8;
+var box9,box10,box11,box12,box13,box14,box15,box16;
+var box17,box18,box19,box20,box21,box22,box23,box24,box25,box26;
 var hero,monster,rope,ground;
 
 function preload() {
@@ -22,14 +24,27 @@ function setup() {
   rope = new Rope(hero.body, { x: 500, y: 50 });
   monster = new Monster(1100,550,300);
 
-  box1 = new Box(600, 100, 70, 70);
+  box1 = new Box(900, 100, 70, 70);
   box2 = new Box(900, 100, 70, 70);
   box3 = new Box(900, 100, 70, 70);
   box4 = new Box(900, 100, 70, 70);
-  box5 = new Box(600, 100, 70, 70);
-  box6 = new Box(600, 100, 70, 70);
-  box7 = new Box(600, 100, 70, 70);
-  box8 = new Box(600, 100, 70, 70);
+  box5 = new Box(900, 100, 70, 70);
+  box6 = new Box(900, 100, 70, 70);
+  box7 = new Box(900, 100, 70, 70);
+  box8 = new Box(800, 100, 70, 70);
+  box9 = new Box(800, 100, 70, 70);
+  box10 = new Box(800, 100, 70, 70);
+  box11 = new Box(800, 100, 70, 70);
+  box12 = new Box(800, 100, 70, 70);
+  box13 = new Box(800, 100, 70, 70);
+  box14 = new Box(800, 100, 70, 70);
+  box15 = new Box(700, 100, 70, 70);
+  box16 = new Box(700, 100, 70, 70);
+  box17 = new Box(700, 100, 70, 70);
+  box18 = new Box(700, 100, 70, 70);
+  box19 = new Box(700, 100, 70, 70);
+  box20 = new Box(700, 100, 70, 70);
+  box21 = new Box(700, 100, 70, 70);
 
 }
 
@@ -45,6 +60,19 @@ function draw() {
   box6.display();
   box7.display();
   box8.display();
+  box9.display();
+  box10.display();
+  box11.display();
+  box12.display();
+  box13.display();
+  box14.display();
+  box15.display();
+  box16.display();
+  box17.display();
+  box18.display();
+  box19.display();
+  box20.display();
+  box21.display();
 
   hero.display();
   rope.display();
@@ -53,14 +81,10 @@ function draw() {
 }
 
 function mouseDragged(){
-      
   Matter.Body.setPosition(hero.body, {x: mouseX , y: mouseY});
-
 }
+
 
 function mouseReleased(){
-  
-  rope.fly();
-
+  hero.fly();
 }
-
